17e67fba6bc4ca50f45a982c9bba3cfcb47280fb
Zonggen Bai <z.bai@mail.utoronto.ca>
2018-08-02 19:58:08 -0400

    Fix syntax

--- clrs-questions/2-4.py
+++ clrs-questions/2-4.py
@@ -16,12 +16,12 @@ def MergeWithInversion (A, p, q, r):
     n1, n2 = q - p + 1, r - q
     L, R = A[p: q+1], A[q+1: r]
     i, j = 1, 1
-    for each k in range (p, r):
+    for k in range (p, r):
         if l[i] <= r[j]:
             A[k] = L[i]
-            i++
+            i += 1
         else:
             A[k] = R[j]
-            j++
+            j += 1
             count += n1 - i + 1
     return count
